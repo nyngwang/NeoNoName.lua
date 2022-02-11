2fe6d2672419dc1ee86a3b70741abadcc66c965f
Ning Wang <nyngwang@gmail.com>
2022-02-11 08:42:49 +0800

    Oops

--- lua/neo-no-name.lua
+++ lua/neo-no-name.lua
@@ -10,13 +10,23 @@ local function get_all_valid_buffers()
   end, vim.api.nvim_list_bufs())
 end
 
+local function find_first_no_name_buf(buffers) -- find a No-Name buffer from the existing ones in `:ls`.
+  if not buffers then
+    buffers = get_all_valid_buffers()
+  end
+  for _, buf in ipairs(buffers) do
+    if (vim.api.nvim_buf_get_name(buf) == '') then
+      return buf end
+  end
+  return nil
+end
+
 local function is_loaded_with_no_name(win)
   return vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(win)) == ''
 end
 ---------------------------------------------------------------------------------------------------
 function M.neo_no_name()
-  local buffers_valid = get_all_valid_buffers()
-  local first_no_name_buf = M.find_first_no_name_buf()
+  local first_no_name_buf = find_first_no_name_buf()
 
   if (first_no_name_buf == nil) then vim.cmd('enew') return end
 
@@ -29,22 +39,11 @@ function M.neo_no_name()
   end
 
   -- delete all the other No-Name buffers.
-  for _, buf in ipairs(buffers_valid) do
+  for _, buf in ipairs(get_all_valid_buffers()) do
     if (vim.api.nvim_buf_get_name(buf) == ''
       and buf ~= first_no_name_buf) then
       vim.cmd('bd ' .. buf) end
   end
-end
-
-function M.find_first_no_name_buf(buffers) -- find a No-Name buffer from the existing ones in `:ls`.
-  if not buffers then
-    buffers = get_all_valid_buffers()
-  end
-  for _, buf in ipairs(buffers) do
-    if (vim.api.nvim_buf_get_name(buf) == '') then
-      return buf end
-  end
-  return nil
 end
 
 local function setup_vim_commands()
